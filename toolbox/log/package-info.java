2aa5cf3a0b358b9e3dc294990a8d3192258e715f
Pedro Reis <ph3dror3is@gmail.com>
2024-07-02 13:24:32 -0300

    Pacote log adicionado ao projeto

---
+++ toolbox/log/package-info.java
@@ -0,0 +1,8 @@
+/*******************************************************************************
+* Classe para criar e imprimir em um arquivo de log.
+*
+* @since 1.0 - 29 de junho de 2024.
+* @version 1.0
+* @author Pedro Reis
+*******************************************************************************/
+package toolbox.log;
